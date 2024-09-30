884b0c61a63d3a590857bea232d4ca2e195a76ec
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-30 10:12:33 +0700

    Add Sidebar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
 
 export default function App() {
   return (
     <div className="sm:-8 relative flex min-h-screen flex-row bg-[#13131a] p-4">
-      <div className="relative mr-10 hidden sm:flex"></div>
+      <div className="relative mr-10 hidden sm:flex">
+        <Sidebar />
+      </div>
 
       <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
         <Routes>
