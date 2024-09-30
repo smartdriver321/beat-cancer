e74b6b71bce1a34459c95bc570d5a7360267aca4
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-30 15:09:18 +0700

    Redirect to login or onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
+import { useStateContext } from './context'
 import { Home, Onboarding } from './pages'
 import { Navbar, Sidebar } from './components'
+import { usePrivy } from '@privy-io/react-auth'
 
 export default function App() {
+  const navigate = useNavigate()
+  const { user, authenticated, ready, login } = usePrivy()
+  console.log(user)
+
+  const { currentUser } = useStateContext()
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login()
+    } else if (user && !currentUser) {
+      navigate('/onboarding')
+    }
+  }, [ready, currentUser, navigate])
+
   return (
     <div className="sm:-8 relative flex min-h-screen flex-row bg-[#13131a] p-4">
       <div className="relative mr-10 hidden sm:flex">
