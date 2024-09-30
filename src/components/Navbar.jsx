e89f4188d6b27936bf09b804f51852d5c0d13d01
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-30 11:27:49 +0700

    Navbar mobile version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { usePrivy } from '@privy-io/react-auth'
 import { IconHeartHandshake } from '@tabler/icons-react'
 
-import { search } from '../assets'
+import { menu, search } from '../assets'
 import CustomButton from './CustomButton'
+import { useNavigate } from 'react-router-dom'
+import { navlinks } from '../constants'
 
 export default function Navbar() {
   const { authenticated, login, user, logout } = usePrivy()
-  console.table('user_info: ', user)
+  const navigate = useNavigate()
+
+  const [isActive, setIsActive] = useState('dashboard')
+  const [toggleDrawer, setToggleDrawer] = useState(false)
 
   const handleLoginLogout = useCallback(() => {
     if (authenticated) {
@@ -50,6 +55,47 @@ export default function Navbar() {
       <div className="relative flex items-center justify-between sm:hidden">
         <div className="flex h-[40px] w-[40px] cursor-pointer items-center justify-center rounded-[10px] bg-[#2c2f32]">
           <IconHeartHandshake size={40} color="#1ec070" className="p-2" />
+        </div>
+        <img
+          src={menu}
+          alt="menu"
+          className="h-[34px] w-[34px] cursor-pointer object-contain"
+          onClick={() => setToggleDrawer((prev) => !prev)}
+        />
+        <div
+          className={`absolute left-0 right-0 top-[60px] z-10 bg-[#1c1c24] py-4 shadow-secondary ${
+            !toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
+                onClick={() => {
+                  setIsActive(link.name)
+                  setToggleDrawer(false)
+                  navigate(link.link)
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.name}
+                  className={`h-[24px] w-[24px] object-contain ${
+                    isActive === link.name ? 'grayscale-0' : 'grayscale'
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue text-[14px] font-semibold ${
+                    isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <div className="mx-4 flex"></div>
         </div>
       </div>
     </div>
